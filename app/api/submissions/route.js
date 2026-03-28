f83db573e54dc48b07862e0de6b46f232455bb79
jahanm1 <devcontainers@RafinThinkPad.localdomain>
2026-03-28 02:23:34 +0200

    fixed bug

--- app/api/submissions/route.js
+++ app/api/submissions/route.js
@@ -6,7 +6,7 @@ import { createClient } from '@/utils/supabase/server';
 
 export async function POST(request) {
   try {
-    const supabase = createClient();
+    const supabase = await createClient();
     const { data: { user } } = await supabase.auth.getUser();
     
     if (!user) {
--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 import { createClient } from '@/utils/supabase/server';
 
 export default async function Home() {
-  const supabase = createClient();
+  const supabase = await createClient();
   const { data: { user } } = await supabase.auth.getUser();
 
   return (
